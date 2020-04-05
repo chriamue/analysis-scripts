b4d4677be4e71b2f947589b32c198c178620597f
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-04-05 18:04:44 +0200

    Generate location csv data

---
+++ analysis/utils/write_location_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from analysis.utils.db import session, LocationModel
+
+
+def load():
+    q = session.query(LocationModel)
+    locations = q.all()
+    all_locations = (loc.serialize() for loc in locations)
+
+    df = pd.DataFrame(all_locations)
+    return df
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+    else:
+        path = "merge-all-days.csv"
+
+    df = load()
+    df.to_csv(path)
